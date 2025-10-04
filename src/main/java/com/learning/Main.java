8d17ebb3a095852f9f807cd46c71988f815d1260
Abhishek Pal <abhi895814@gmail.com>
2025-10-04 19:18:38 +0530

    Initial commit

---
+++ src/main/java/com/learning/Main.java
@@ -0,0 +1,82 @@
+package com.learning;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import com.learning.entity.Employee;
+
+public class Main {
+	
+	static Metadata meta = null;
+	
+	public static SessionFactory getSessionFactory() {
+		return new MetadataSources(new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build())
+				.getMetadataBuilder().build().buildSessionFactory();
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Employee e = new Employee("Sonu", "male", 50000);	
+//		Configuration cfg = new Configuration().configure("hibernate.cfg.xml"); //here I made configuration.
+		
+
+		SessionFactory sessionFactory = getSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+		session.persist(e); 
+		tx.commit();
+		System.out.println("Main.main");
+		
+	}
+	
+}
+
+
+//pancham's code.
+//package com.learning;
+//
+//import org.hibernate.Session;
+//import org.hibernate.SessionFactory;
+//import org.hibernate.Transaction;
+//import org.hibernate.boot.Metadata;
+//import org.hibernate.boot.MetadataSources;
+//import org.hibernate.boot.registry.StandardServiceRegistry;
+//import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+//import org.hibernate.cfg.Configuration;
+//
+//import com.learning.entity.Employee;
+//
+//public class Main {
+//
+//	public static SessionFactory getSessionFactory() {
+//		return new MetadataSources(new StandardServiceRegistryBuilder().configure("hibernate.cfg1.xml").build())
+//				.getMetadataBuilder().build().buildSessionFactory();
+//	}
+//
+//	public static void main(String[] args) {
+//
+//		Employee e = new Employee("Chandra", "male", 50000);
+////		1st
+////		Configuration cfg = new Configuration().configure("hibernate.cfg1.xml");
+//
+////		2nd
+////		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg1.xml").build();
+////		Metadata metadata = new MetadataSources(ssr).getMetadataBuilder().build();
+////		SessionFactory sessionFactory = metadata.buildSessionFactory();
+//		
+//		SessionFactory sf = getSessionFactory();
+//		Session session = sf.openSession();
+//		Transaction tx = session.beginTransaction();
+//		session.persist(e);
+//		tx.commit();
+//		System.out.println("Main.main()");
+//
+//	}
+//
+//}
